3d8e7641a343db6275ef4af9a92d6b4348033803
Mujahid2000 <developermujahid2001@gmail.com>
2023-12-20 18:01:01 +0600

    add update code

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    
 
     const DataCollection = client.db("Mobile-Shop").collection('MobileData');
     const cartCollection = client.db("Mobile-Shop").collection('Cart');
